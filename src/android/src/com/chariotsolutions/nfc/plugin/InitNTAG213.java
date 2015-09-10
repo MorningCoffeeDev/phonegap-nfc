8adacea3b6c166e2e4edfa04c67df27af253f90c
xueshibing <xueshibing@gmail.com>
2015-09-10 19:46:00 +0800

    reorder initNTAG213

--- src/android/src/com/chariotsolutions/nfc/plugin/InitNTAG213.java
+++ src/android/src/com/chariotsolutions/nfc/plugin/InitNTAG213.java
@@ -9,7 +9,7 @@ import java.io.IOException;
  * Created by kevin on 5/27/15.
  */
 public class InitNTAG213 {
-    private static final String TAG = "InitNTAG213" ;
+    private static final String TAG = "InitNTAG213";
     private MifareUltralight mc;
     private String password;
 
@@ -22,27 +22,27 @@ public class InitNTAG213 {
         byte[] type = new byte[]{(byte) 0xA2, (byte) 0x2A, (byte) 0x80, (byte) 0x00, (byte) 0x00, (byte) 0x00};
         byte[] pwd = new byte[]{(byte) 0xA2, (byte) 0x2B, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF};
         if (this.password != null && !"".equals(this.password)) {
-            byte[] auth = Util.convertHexAsciiToByteArray(this.password,4);
-            System.arraycopy(auth,0,pwd,2,4);
+            byte[] auth = Util.convertHexAsciiToByteArray(this.password, 4);
+            System.arraycopy(auth, 0, pwd, 2, 4);
         }
         byte[] pack = new byte[]{(byte) 0xA2, (byte) 0x2C, (byte) 0x33, (byte) 0x33, (byte) 0x00, (byte) 0x00};
 
         byte[] range = new byte[]{(byte) 0xA2, (byte) 0x29, (byte) 0x04, (byte) 0x00, (byte) 0x00, (byte) 0x04};
 
-        if (transmit(type) && transmit(pwd) && transmit(pack) && transmit(range)) {
+        if (transmit(type) && transmit(pack) && transmit(range) && transmit(pwd)) {
             return true;
         } else {
             return false;
         }
     }
 
-    public boolean transmit(byte[] cmd){
-        Log.d(TAG,"CMD----------");
+    public boolean transmit(byte[] cmd) {
+        Log.d(TAG, "CMD----------");
         Log.d(TAG, Util.ByteArrayToHexString(cmd));
 
         try {
             byte[] response = this.mc.transceive(cmd);
-            Log.d(TAG,Util.ByteArrayToHexString(response));
+            Log.d(TAG, Util.ByteArrayToHexString(response));
         } catch (IOException e) {
             e.printStackTrace();
             return false;
